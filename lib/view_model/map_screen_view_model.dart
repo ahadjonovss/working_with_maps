43c7e9fa9570a5b24adf66333c1ed0acf8727b1a
Samandar <samandarahadjonov@gmail.com>
2022-12-27 17:31:14 +0500

    maps view model done

--- lib/view_model/map_screen_view_model.dart
+++ lib/view_model/map_screen_view_model.dart
@@ -1,3 +1,19 @@
-class MapScreenViewModel extends ChangeNotifier{
+import 'package:flutter/material.dart';
+import 'package:working_with_maps/data/repositories/geocoding_repository.dart';
 
+import '../data/models/latlong_model.dart';
+
+class MapScreenViewModel extends ChangeNotifier{
+  MapScreenViewModel({required this.geocodingRepo});
+
+  final GeocodingRepo geocodingRepo;
+
+  String addressText = "";
+
+  fetchAddress({required LatLong latLong, required String kind}) async {
+    print("CAME1");
+    addressText = await geocodingRepo.getAddress(latLong, kind);
+    print("CAM2");
+    notifyListeners();
+  }
 }
