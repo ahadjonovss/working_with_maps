db07474e47449a94cc2288c6fb5f7199f24257b6
Samandar <samandarahadjonov@gmail.com>
2022-12-27 14:01:58 +0500

    getting location

--- lib/view_model/splash_view_model.dart
+++ lib/view_model/splash_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:working_with_maps/data/models/latlong_model.dart';
+import 'package:location/location.dart';
 
 class SplashViewModel extends ChangeNotifier{
 
@@ -8,8 +9,35 @@ class SplashViewModel extends ChangeNotifier{
   }
 
   LatLong? latLong;
+  bool _serviceEnabled = false;
+  PermissionStatus _permissionGranted = PermissionStatus.denied;
+  Location location = Location();
 
-  fetchCurrentLocation(){
+  fetchCurrentLocation() async {
 
+    _serviceEnabled = await location.serviceEnabled();
+
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    LocationData locationData = await location.getLocation();
+    latLong = LatLong(
+      lat: locationData.latitude!,
+      long: locationData.longitude!,
+    );
+    await Future.delayed(const Duration(seconds: 3));
+    notifyListeners();
   }
 }
